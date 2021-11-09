23fe26c01cf17f90dbf757ed944c3e1dcff9da37
LovelyA72 <a72@luotianyi.pro>
2021-11-09 16:04:13 -0500

    add config.h to gitignore

--- src/config.h
+++ src/config.h
@@ -2,10 +2,10 @@
 #define INCLUDE_GUARD
 
 #define PROJECT_NAME "neoworld"
-#define PROJECT_VER  "0.2.9"
+#define PROJECT_VER  "0.2.10"
 #define PROJECT_VER_MAJOR "0"
 #define PROJECT_VER_MINOR "2"
-#define PTOJECT_VER_PATCH "9"
-#define PTOJECT_GITHASH "c226486"
+#define PTOJECT_VER_PATCH "10"
+#define PTOJECT_GITHASH "9ff0edc"
 
 #endif // INCLUDE_GUARD
